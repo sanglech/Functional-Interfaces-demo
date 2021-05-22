5dad84ffab00e97380687cfd621096704574822f
Christian <christian@sanglech.com>
2021-05-22 21:33:44 +0900

    Function example for first name and last name

--- src/com/christian/Main.java
+++ src/com/christian/Main.java
@@ -3,6 +3,7 @@ package com.christian;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -16,7 +17,7 @@ public class Main {
         Employee emp2= new Employee("john",31);
         Employee emp3= new Employee("christian",41);
         Employee emp4= new Employee("Bob",50);
-        Employee emp5= new Employee("mary",15);
+        Employee emp5= new Employee("mary sue",15);
 
         List<Employee> employees= new ArrayList<>();
 
@@ -64,6 +65,23 @@ public class Main {
         }
 
 
+        //Function example
+        Function<Employee,String> getEmpLastName= (Employee emp)->{
+            return emp.getName().substring(emp.getName().indexOf(' ')+1);
+        };
+
+        Function<Employee,String> getEmpFirstName= (Employee emp)->{
+            return emp.getName().substring(0,emp.getName().indexOf(' '));
+        };
+
+        //String lastName= getEmpLastName.apply(employees.get(4));
+        //System.out.println(lastName);
+
+
+        String firstName=getName(getEmpFirstName,employees.get(4));
+        String lastName=getName(getEmpLastName,employees.get(4));
+
+
         //System.out.println("Employees over 30");
        // System.out.println("=================");
 //        employees.forEach(emp->{
@@ -80,6 +98,11 @@ public class Main {
 //        }
 
 
+    }
+
+
+    private static String getName(Function<Employee,String> getName,Employee employee){
+        return getName.apply(employee);
     }
 
     private static void printEmployeesByAge(List<Employee> employees,
